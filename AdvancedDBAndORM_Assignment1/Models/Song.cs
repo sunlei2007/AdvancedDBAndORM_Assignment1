fd39b26ccf40d4396f6e9e8ddc847d3d4c5c860e
Lei Sun <139188566@qq.com>
2023-03-19 11:59:02 -0500

    M

--- AdvancedDBAndORM_Assignment1/Models/Song.cs
+++ AdvancedDBAndORM_Assignment1/Models/Song.cs
@@ -1,8 +1,12 @@
-﻿namespace AdvancedDBAndORM_Assignment1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AdvancedDBAndORM_Assignment1.Models
 {
     public class Song
     {
+        
         public int ID { get; set; }
+        
         public string Name { get; set; }
 
         public Song(string name) 
